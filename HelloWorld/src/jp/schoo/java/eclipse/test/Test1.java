b4381a2c005de4d6963c61cd07be83769a0b872b
Sumudeshu <13051565357@163.com>
2024-04-05 01:06:40 +0900

    test

--- HelloWorld/src/jp/schoo/java/eclipse/test/Test1.java
+++ HelloWorld/src/jp/schoo/java/eclipse/test/Test1.java
@@ -1,24 +1,18 @@
 package jp.schoo.java.eclipse.test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import jp.schoo.java.eclipse.Student;
-
 class Test1 {
 
-	@Test
-	void test1() {
-		Student s = new Student();
-		s.setId(123);
-		System.out.println(s.getId());
-	}
-	
-	@Test
-	void test2() {
-		Student s = new Student();
-		s.setName("abc");
-		System.out.println(s.getName());
-	}
+//	@Test
+//	void test1() {
+//		Student s = new Student();
+//		s.setId(123);
+//		System.out.println(s.getId());
+//	}
+//	s
+//	@Test
+//	void test2() {
+//		Student s = new Student();
+//		s.setName("abc");
+//		System.out.println(s.getName());
+//	}
 }
